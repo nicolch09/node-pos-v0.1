95e06506a42a803c4661340d79c71eb91b2816af
CHRISTOPER NICOLAS (DOCU-IRIS-ISD-OOCLL/MNL) <NICOLCH@OOCL.COM>
2019-10-07 17:45:35 +0800

    FINAL COMPLETION

--- main/main.js
+++ main/main.js
@@ -1,29 +1,45 @@
 module.exports = function main(inputs) {
-    var store = [];
     var total = 0;
-    var current = '';
+    var currentName = '';
+    var unitPrice = 0;
+    var unit = '';
+    var totalBottles = 0;
+    var subTotal = 0;
     var receipt = '***<store earning no money>Receipt ***\n';
-    for (var x = 0; x < inputs.length; x++) {
-        var totalBottles = 0;
-        var unitPrice = inputs[x].Price;
-        var subTotal = 0;
-        for (var y = x; y < inputs.length; y++) { 
-            if(inputs[y].Name == inputs[x].Name){
-                totalBottles++;
-                subTotal += unitPrice;
+    for (var x = 0; x < inputs.length; x++) {    
+        if(currentName == ''){
+            currentName = inputs[x].Name;
+            unitPrice = inputs[x].Price;
+            unit = inputs[x].Unit;
+        }
+        if(inputs[x].Name == currentName){
+            totalBottles++;
+            subTotal += unitPrice;
+            if(x + 1 == inputs.length){
+                total += subTotal;
+                if(unit == 'bottle'){
+                    receipt += 'Name: ' + currentName + ', Quantity: ' + totalBottles + ' ' + unit + 's, Unit price: ' + 
+                    unitPrice + '.00 (yuan), Subtotal: '  + subTotal + '.00 (yuan)\n';
+                } else{
+                    receipt += 'Name: ' + currentName + ', Quantity: ' + totalBottles + ', Unit price: ' + 
+                    unitPrice + '.00 (yuan), Subtotal: '  + subTotal + '.00 (yuan)\n';
+                }
+            }
+        } else{
+            total += subTotal;
+            if(unit == 'bottle'){
+                receipt += 'Name: ' + currentName + ', Quantity: ' + totalBottles + ' ' + unit + 's, Unit price: ' + 
+                unitPrice + '.00 (yuan), Subtotal: '  + subTotal + '.00 (yuan)\n';
             } else{
-                total += subTotal;
-                receipt += 'Name: ' + inputs[x].Name + ', Quantity: ' + totalBottles + ', Unit price: ' + unitPrice + '.00 (yuan), Subtotal: '  + subTotal + ' (yuan)\n';
-                x = y - 1;
-                break;
+                receipt += 'Name: ' + currentName + ', Quantity: ' + totalBottles + ', Unit price: ' + 
+                unitPrice + '.00 (yuan), Subtotal: '  + subTotal + '.00 (yuan)\n';
             }
-            //for last sequence
-            if(y + 1 == inputs.length){
-                total += subTotal;
-                receipt += 'Name: ' + inputs[x].Name + ', Quantity: ' + totalBottles + ', Unit price: ' + unitPrice + '.00 (yuan), Subtotal: '  + subTotal + ' (yuan)\n';
-                x = y - 1;
-                break;
-            }
+            x -= 1;
+            currentName = '';
+            unitPrice = '';
+            unit = '';
+            totalBottles = 0;
+            subTotal = 0;
         }
         if(x + 1 == inputs.length){
             receipt += '----------------------\n';
